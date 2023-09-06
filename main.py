72e054b04e47065559a571c02bc5a08505e91454
Tianji Rao <rao@TTRao.local>
2023-09-06 15:26:52 -0400

    change main & test_main

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def desc_df(df):
+import pandas as pd
+
+def desc_df():
+    data = {'price': [22000, 27000, 25000, 29000, 35000]
+        }
+    df = pd.DataFrame(data)
     return df.describe()
--- test_main.py
+++ test_main.py
@@ -1,13 +1,8 @@
 from main import desc_df
 import pandas as pd
 
-data = {'price': [22000, 27000, 25000, 29000, 35000]
-        }
-
-df = pd.DataFrame(data)
-
-def test_main(df):
-    desc = desc_df(df)
+def test_main():
+    desc = desc_df()
     assert desc.loc['count'][0] == 5.0
     assert desc.loc['mean'][0] == 27600.0
     assert desc.loc['min'][0] == 22000.0
