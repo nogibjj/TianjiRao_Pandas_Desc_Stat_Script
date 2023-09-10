cd03103cf21530ef6199093ebb701eca91ed5faa
TianoRao <104114843+TianoRao@users.noreply.github.com>
2023-09-10 00:32:33 +0000

    change main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # import pandas as pd
+import matplotlib as plt
 
 def desc_df(df):
     return df.describe()
 
-def bar_plot(df):
-    df.plot(kind='bar')
 
+def bar_plot(df):
+    df.plot(kind="bar")
+    plt.show()
