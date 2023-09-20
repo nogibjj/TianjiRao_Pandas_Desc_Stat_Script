5f7f62faaf5fb45b3808614ee7751360b9e249ff
Tianji Rao <rao@TTRao.local>
2023-09-20 16:57:48 -0400

    add actions

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ def desc_df(df):
 def bar_plot(df):
     df.plot(kind="bar")
     plt.show()
+    
+    
+def bar_plot_save(df):
+    df.plot(kind="bar")
+    plt.savefig("bar.png")
+    
+def save_to_md(df):
+    tab1 = df.describe()
+    bar_plot_save(df)
+    with open("congress_summary.md", "w", encoding="utf-8") as file:
+        file.write("Statistical Describe:\n")
+        file.write(tab1)
+        file.write("\n\n") 
+        file.write("![Bar Plot](bar.png)\n")
